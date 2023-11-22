4c24af5f4c856cb5a0af387849ca6bcf0635f427
tlannigan <tristanlannigan@gmail.com>
2023-11-21 21:35:58 -0700

    Fix spacing and add hovered underlines in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import Navbar from './Navbar'
 export class Header extends Component {
   render() {
     return (
-      <div className='flex justify-between'>
-        <div>Reliable Painters</div>
+      <div className='flex justify-between py-8'>
+        <div>
+          <h1 className='rowdies text-3xl'>RELIABLE PAINTERS</h1>
+        </div>
         <Navbar/>
       </div>
     )
