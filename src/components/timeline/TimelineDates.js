077493b2400f1510eaaa8f0a5b392d4e8b944d7c
tlannigan <tristanlannigan@gmail.com>
2023-11-21 20:54:46 -0700

    Add timeline skeleton

---
+++ src/components/timeline/TimelineDates.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TimelineDates extends Component {
+  render() {
+    return (
+      <div>TimelineDates</div>
+    )
+  }
+}
+
+export default TimelineDates
