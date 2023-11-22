120404802b88bf843163b60d63623f9b21348cba
tlannigan <tristanlannigan@gmail.com>
2023-11-21 20:54:35 -0700

    Add more components to app layout
    - Change title to "Reliable Painters"
    - Add background colour and rounded corners to sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Estimate from './components/Estimate';
+import DividingHeader from './components/DividingHeader';
+import EstimateSection from './components/estimates/EstimateSection';
 import Header from './components/Header';
+import ReviewSection from './components/ReviewSection';
 
 function App() {
   return (
     <div className="App max-w-screen-lg mx-auto">
       <Header />
-      <Estimate />
+      <EstimateSection />
+
+      <DividingHeader text="Hi, I'm Nathan, a master painter" />
+      <ReviewSection />
+
+      <DividingHeader text="With over 50 years of experience" />
     </div>
   );
 }
