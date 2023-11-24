84e6c267fe0d931ab69bf4ac91a24ca4833ac6c2
tlannigan <tristanlannigan@gmail.com>
2023-11-23 21:56:34 -0700

    Add Material UI dependency

--- src/components/estimates/EstimateForm.js
+++ src/components/estimates/EstimateForm.js
@@ -1,31 +1,27 @@
+import { TextField } from '@mui/material'
 import React, { Component } from 'react'
 
 export class EstimateForm extends Component {
   render() {
     return (
-      <div className='flex flex-col basis-4/12 justify-between section'>
-        <div>
+      <div className='flex flex-col basis-4/12 justify-between gap-y-8 section'>
+        <div className='text-xl roboto-bold'>
             <h3>Request a Free Estimate</h3>
         </div>
-        <div className='flex flex-col'>
-            <label for='estimate-first-name'>First name</label>
-            <input name='estimate-first-name' type='text' placeholder='John' />
+        <div className=''>
+          <TextField fullWidth id='estimate-first-name' label='First name' variant='outlined' defaultValue={'John'} />
         </div>
-        <div className='flex flex-col'>
-            <label for='estimate-last-name'>Last name</label>
-            <input name='estimate-last-name' type='text' placeholder='Smith' />
+        <div className=''>
+          <TextField fullWidth id='estimate-last-name' label='Last name' variant='outlined' defaultValue={'Smith'} />
         </div>
-        <div className='flex flex-col'>
-            <label for='estimate-email'>Email address</label>
-            <input name='estimate-email' type='text' placeholder='john.smith@gmail.com' />
+        <div className=''>
+          <TextField fullWidth id='estimate-email' label='Email address' variant='outlined' defaultValue={'john.smith@gmail.com'} />
         </div>
-        <div className='flex flex-col'>
-            <label for='estimate-phone'>Phone number</label>
-            <input name='estimate-phone' type='text' placeholder='123-456-7890' />
+        <div className=''>
+          <TextField fullWidth id='estimate-phone' label='Phone number' variant='outlined' defaultValue={'123-456-7890'} />
         </div>
-        <div className='flex flex-col'>
-            <label for='estimate-details'>Details</label>
-            <input name='estimate-details' type='text' placeholder='My bathroom needs repainting...' />
+        <div className=''>
+          <TextField fullWidth id='estimate-details' label='Details' variant='outlined' defaultValue={'My bathroom needs repainting...'} />
         </div>
         <div>
             <button>Request Estimate</button>
