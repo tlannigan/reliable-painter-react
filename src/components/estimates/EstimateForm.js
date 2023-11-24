5ba2817c51ad7065d98e8398c8fb95ce24310825
tlannigan <tristanlannigan@gmail.com>
2023-11-23 22:07:16 -0700

    Add form field placeholders
    - Fix EstimateImage height fit

--- src/components/estimates/EstimateForm.js
+++ src/components/estimates/EstimateForm.js
@@ -9,19 +9,19 @@ export class EstimateForm extends Component {
             <h3>Request a Free Estimate</h3>
         </div>
         <div className=''>
-          <TextField fullWidth id='estimate-first-name' label='First name' variant='outlined' defaultValue={'John'} />
+          <TextField fullWidth id='estimate-first-name' label='First name' variant='outlined' placeholder={'John'} />
         </div>
         <div className=''>
-          <TextField fullWidth id='estimate-last-name' label='Last name' variant='outlined' defaultValue={'Smith'} />
+          <TextField fullWidth id='estimate-last-name' label='Last name' variant='outlined' placeholder={'Smith'} />
         </div>
         <div className=''>
-          <TextField fullWidth id='estimate-email' label='Email address' variant='outlined' defaultValue={'john.smith@gmail.com'} />
+          <TextField fullWidth id='estimate-email' label='Email address' variant='outlined' placeholder={'john.smith@gmail.com'} />
         </div>
         <div className=''>
-          <TextField fullWidth id='estimate-phone' label='Phone number' variant='outlined' defaultValue={'123-456-7890'} />
+          <TextField fullWidth id='estimate-phone' label='Phone number' variant='outlined' placeholder={'123-456-7890'} />
         </div>
         <div className=''>
-          <TextField fullWidth id='estimate-details' label='Details' variant='outlined' defaultValue={'My bathroom needs repainting...'} />
+          <TextField fullWidth id='estimate-details' label='Details' variant='outlined' placeholder={'My bathroom needs repainting...'} />
         </div>
         <div>
             <button>Request Estimate</button>
