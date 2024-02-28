01a524cef819b34382ed73cd7006779e48f52ddc
tlannigan <tristanlannigan@gmail.com>
2024-02-27 20:31:29 -0700

    Fix styling for BBB SVG and request estimate button

--- src/components/estimates/EstimateForm.js
+++ src/components/estimates/EstimateForm.js
@@ -1,12 +1,13 @@
-import { TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import React, { Component } from 'react'
+import bbbImage from '../../assets/images/bbb_a_plus_rating.svg';
 
 export class EstimateForm extends Component {
   render() {
     return (
       <div className='flex flex-col basis-4/12 justify-between gap-y-8 section'>
         <div className='text-xl roboto-bold'>
-            <h3>Request a Free Estimate</h3>
+          <h3>Request a Free Estimate</h3>
         </div>
         <div className=''>
           <TextField fullWidth id='estimate-first-name' label='First name' variant='outlined' placeholder={'John'} />
@@ -23,8 +24,13 @@ export class EstimateForm extends Component {
         <div className=''>
           <TextField fullWidth id='estimate-details' label='Details' variant='outlined' placeholder={'My bathroom needs repainting...'} />
         </div>
-        <div>
-            <button>Request Estimate</button>
+        <div className='flex items-center justify-between'>
+          <div className='flex-auto'>
+            <img src={bbbImage} alt='BBB A+ rating' className='svg-filter h-[36px]' />
+          </div>
+          <div className='basis-2/5'>
+            <Button fullWidth size="large" variant='contained' className='!text-sm'>REQUEST</Button>
+          </div>
         </div>
       </div>
     )
